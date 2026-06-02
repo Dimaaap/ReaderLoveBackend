7645dd8ef18066bf7974b8157fb7d7dffcde4dcf
Dimaaap <procdima49@gmail.com>
2026-06-02 20:43:01 +0300

    Added new endpoint

--- entities/social_links/views.py
+++ entities/social_links/views.py
@@ -7,7 +7,7 @@ from core.redis_config import redis_client
 from core.models import db_helper
 from entities.social_links.schema import SocialLinkSchema
 
-from .crud import *
+from . import crud
 
 router = APIRouter(tags=["Social Links"])
 
@@ -23,7 +23,7 @@ async def get_all(
     if cached:
         return json.loads(cached)
 
-    data = await get_all_social_links(session)
+    data = await crud.get_all_social_links(session)
     result = [SocialLinkSchema.model_validate(item) for item in data]
 
     await redis_client.set(
@@ -31,3 +31,23 @@ async def get_all(
     )
 
     return result
+
+
+@router.get("/{social_link_id}")
+async def get_social_link_by_id(
+    social_link_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    cache_key = f"social_links:{social_link_id}"
+
+    cached = await redis_client.get(cache_key)
+
+    if cached:
+        return json.loads(cached)
+
+    data = await crud.get_social_link_by_id(session, social_link_id)
+
+    if data:
+        await redis_client.set(cache_key, SocialLinkSchema.model_validate(data), ex=300)
+
+    return data
